1f5bec52fc328e77cb979c94fc28e2c3b4b20db3
ByeongGil Jung <jbkcose@gmail.com>
2018-07-26 17:23:13 +0900

    [18-07-26] - adding the notification

--- controller.py
+++ controller.py
@@ -12,6 +12,7 @@
 from flask import Flask, render_template, request, Response
 import json
 import execute as ec
+import time
 
 app = Flask(__name__)
 
@@ -26,7 +27,7 @@ def hello_world():
 
 
 @app.route('/input', methods=["POST", "GET"])
-def upload_file():
+def register_user():
     user = None
     repo = None
 
@@ -38,12 +39,59 @@ def upload_file():
         user = request.args.get("user")
         repo = request.args.get("repo")
 
-    git_readme, issue_list, full_request_list = ec.execute(user, repo)
+    git_readme, issue_list, pull_request_list = ec.execute(user, repo)
 
-    response_ = json.dumps({"git_readme": git_readme, "issue_list": issue_list[0], "full_request_list": full_request_list[0]})
+    response_ = json.dumps({"git_readme": git_readme, "issue_list": issue_list, "pull_request_list": pull_request_list})
+
+    return Response(response_, status=200, mimetype="application/json")
+
+
+@app.route('/noti', methods=["POST"])
+def notificate():
+    git_readme = None
+    issue_list = None
+    pull_request_list = None
+
+    issue_len = None
+    pull_req_len = None
+    user = None
+    repo = None
+
+    noti_check = True
+    sumething_changed = None
+
+    if request.method == "POST":
+        issue_len = int(request.form["issue_len"])
+        pull_req_len = int(request.form["pull_req_len"])
+        user = request.form["user"]
+        repo = request.form["repo"]
+
+    while noti_check:
+        git_readme, issue_list, pull_request_list = ec.execute(user, repo)
+
+        current_issue_count = len(issue_list)
+        current_pull_req_count = len(pull_request_list)
+
+        if issue_len is not current_issue_count:
+            noti_check = False
+            issue_len = current_issue_count
+            sumething_changed = "Issue"
+        elif pull_req_len is not current_pull_req_count:
+            noti_check = False
+            pull_req_len = current_pull_req_count
+            sumething_changed = "Pull Request"
+
+        time.sleep(3)
+
+    response_ = json.dumps({
+        "git_readme": git_readme,
+        "issue_list": issue_list,
+        "pull_request_list": pull_request_list,
+        "something_changed": sumething_changed
+    })
 
     return Response(response_, status=200, mimetype="application/json")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8030)
+    app.run(debug=True, port=8011)
